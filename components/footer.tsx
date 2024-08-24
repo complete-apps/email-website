b6a5ec3a702e2fbdd807349f5e20c7f09df9e397
Archit Chandra <architchandra@gmail.com>
2024-08-24 19:31:58 +0530

    Update footer copy

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="max-w-prose mx-auto text-center">
             <p className="mt-12 text-sm">
-                Copyright &copy; 2024 Aldous Tech LLC
+                Copyright &copy; 2024 Complete Apps (Aldous Tech LLC)
                 <br />
                 All rights reserved
             </p>
